282bee0adff64eeeb7ecdc435fdf053f2680379e
Sofyan Thayf <sofyanthayf@gmail.com>
2016-07-20 16:06:55 +0800

    initial
    first copy of files

---
+++ contoh/conn.php
@@ -0,0 +1,10 @@
+<?php
+mysql_connect("localhost","BajiAteka20","");
+mysql_select_db("contohperpus");
+
+//fungsi format rupiah 
+/**function format_rupiah($rp) {
+	$hasil = "Rp." . number_format($rp, 0, "", ".") . ",00";
+	return $hasil;
+    }**/
+?>
